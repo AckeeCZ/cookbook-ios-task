bddf3021ebe493c73aa299de14e90fed8ae33a77
Michal Štembera <stembera.michal@gmail.com>
2017-11-07 16:46:41 +0100

    Remove Cocoapods from future tracking.

--- Pods/ReactiveCocoa/ReactiveCocoa/UIKit/UIBarItem.swift
+++
@@ -1,9 +0,0 @@
-import ReactiveSwift
-import UIKit
-
-extension Reactive where Base: UIBarItem {
-	/// Sets whether the bar item is enabled.
-	public var isEnabled: BindingTarget<Bool> {
-		return makeBindingTarget { $0.isEnabled = $1 }
-	}
-}
